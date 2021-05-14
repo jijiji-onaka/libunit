2701cf64a184c2ae31a1173686b8dadbb5295900
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-05-15 04:45:02 +0900

    add testcase

--- tests/toupper_test/15_basic14_test.c
+++ tests/toupper_test/15_basic14_test.c
@@ -6,13 +6,13 @@
 /*   By: tjinichi <tjinichi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/15 04:35:49 by tjinichi          #+#    #+#             */
-/*   Updated: 2021/05/15 04:43:20 by tjinichi         ###   ########.fr       */
+/*   Updated: 2021/05/15 04:45:00 by tjinichi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../tester.h"
 
-int	toupper_basic1_test(void)
+int	toupper_basic14_test(void)
 {
 	g_failed_testcase = "if (toupper(42) != ft_toupper(42))\
 	return (0);\
